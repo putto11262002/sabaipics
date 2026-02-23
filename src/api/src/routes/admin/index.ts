0596c877cfa731654e91eeb19d164e3d88cc0718
Put Suthisrisinlpa <putto11262002@gmail.com>
2026-02-23 14:07:53 +0700

    feat: admin CRUD endpoints for in-house gift codes

--- src/api/src/routes/admin/index.ts
+++ src/api/src/routes/admin/index.ts
@@ -4,6 +4,7 @@ import { creditTokensRouter } from "./credit-tokens";
 import { giftCodesRouter } from "./gift-codes";
 import { discountCodesRouter } from "./discount-codes";
 import { adminUsersRouter } from "./users";
+import { giftCodesV2Router } from "./gift-codes-v2";
 import type { Env } from "../../types";
 
 export const adminRouter = new Hono<Env>()
@@ -11,4 +12,5 @@ export const adminRouter = new Hono<Env>()
   .route("/credit-tokens", creditTokensRouter)
   .route("/gift-codes", giftCodesRouter)
   .route("/discount-codes", discountCodesRouter)
-  .route("/users", adminUsersRouter);
+  .route("/users", adminUsersRouter)
+  .route("/gift-codes-v2", giftCodesV2Router);
